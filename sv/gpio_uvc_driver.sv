55b507fa13a8e03013b8249ef6165db7105fd528
Nami-L <namigtle066@gmail.com>
2025-10-13 14:48:03 -0600

    feat: add sync and async tasks to drive

--- sv/gpio_uvc_driver.sv
+++ sv/gpio_uvc_driver.sv
@@ -9,9 +9,11 @@ class gpio_uvc_driver extends uvm_driver #(gpio_uvc_sequence_item);
   gpio_uvc_config     m_config;
 
   extern function new(string name, uvm_component parent);
-  
+
   extern task run_phase(uvm_phase phase);
   extern task do_drive();
+  extern task drive_sync();
+  extern task drive_async();
 
 endclass : gpio_uvc_driver
 
@@ -30,8 +32,32 @@ task gpio_uvc_driver::run_phase(uvm_phase phase);
 endtask : run_phase
 
 
+task gpio_uvc_driver::drive_sync();
+  @(vif.cb_drv);
+  vif.cb_drv.gpio_pin <= req.m_gpio_pin;
+endtask : drive_sync
+
+
+task gpio_uvc_driver::drive_async();
+  vif.gpio_pin = req.m_gpio_pin;
+
+  if (req.m_delay_enable == GPIO_UVC_ITEM_DELAY_ON) begin
+    #(req.m_delay_duration_ps * 1ps);
+  end else begin
+    @(vif.cb_drv);
+  end
+endtask : drive_async
+
+
 task gpio_uvc_driver::do_drive();
+
+  if (req.m_trans_type == GPIO_UVC_ITEM_ASYNC) begin
+    drive_async();
+  end else begin
+    drive_sync();
+  end
+
   `uvm_info(get_type_name(), "PUT THE DRIVER CODE HERE", UVM_MEDIUM)
 endtask : do_drive
 
-`endif // GPIO_UVC_DRIVER_SV
+`endif  // GPIO_UVC_DRIVER_SV
