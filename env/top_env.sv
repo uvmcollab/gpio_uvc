332fa74820e27798119e561c8e009e8a92c060ff
Nami-L <namigtle066@gmail.com>
2025-11-24 22:37:51 -0600

    feat: add config and agente for port c

--- env/top_env.sv
+++ env/top_env.sv
@@ -16,6 +16,9 @@ class top_env extends uvm_env;
   gpio_uvc_config   m_port_b_config;
   gpio_uvc_agent    m_port_b_agent;
 
+  gpio_uvc_config   m_port_c_config;
+  gpio_uvc_agent    m_port_c_agent;
+
   top_vsqr          vsqr;
 
   //top_scoreboard    m_scoreboard;
@@ -80,6 +83,19 @@ function void top_env::build_phase(uvm_phase phase);
   uvm_config_db #(gpio_uvc_config)::set(this, "m_port_b_agent", "config", m_port_b_config);
   m_port_b_agent = gpio_uvc_agent::type_id::create("m_port_b_agent", this);
 
+    // ========================== PORT C AGENT ========================== //
+  m_port_c_config = gpio_uvc_config::type_id::create("m_port_b_config");
+  m_port_c_config.is_active = UVM_ACTIVE;
+  m_port_c_config.gpio_width = 'd8;
+  m_port_c_config.start_value = 'd0;
+
+  if (!uvm_config_db#(virtual gpio_uvc_if)::get(this, "m_port_c_agent", "vif", m_port_c_config.vif)) begin
+    `uvm_fatal(get_name(), "Could not retrieve m_port_c_config from config db")
+  end
+
+  uvm_config_db #(gpio_uvc_config)::set(this, "m_port_c_agent", "config", m_port_c_config);
+  m_port_c_agent = gpio_uvc_agent::type_id::create("m_port_c_agent", this);
+
   // ================================= COVERAGE =============================== //
   //m_coverage  = top_coverage::type_id::create("m_coverage", this);
 
@@ -97,6 +113,7 @@ function void top_env::connect_phase(uvm_phase phase);
   vsqr.m_port_rst_sequencer  = m_port_rst_agent.m_sequencer;
   vsqr.m_port_a_sequencer = m_port_a_agent.m_sequencer;
   vsqr.m_port_b_sequencer = m_port_b_agent.m_sequencer;
+  vsqr.m_port_c_sequencer = m_port_c_agent.m_sequencer;
 
 
   // ========================= SCOREBOARD CONNECTIONS ========================= //
